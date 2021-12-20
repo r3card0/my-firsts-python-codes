0fa05251e96cd781b7e60ec1a68b9e0fe3f1f793
r3card0 <ricardo.rdz@pm.me>
2021-12-20 17:12:58 -0600

    Add a planet list. The name of the planets are printed in a reverse way

---
+++ planet_list.py
@@ -0,0 +1,8 @@
+def run():
+    planets = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune']
+    for planet in planets:
+        print(planet[::-1])
+
+
+if __name__ == '__main__':
+    run()
