29d169dfa87cdc216e58f8c7ff1eef8d7a2aa013
r3card0 <ricardo.rdz@pm.me>
2021-12-20 18:21:06 -0600

    Add a tuple. Mohs scale of hardness

---
+++ mohs-scale-tuple.py
@@ -0,0 +1,17 @@
+# Mohs scale of hardness
+def run():
+    print('Mohs scale of hardness:')
+
+    
+    def mohs_scale():
+        mohs = ('1.Talc','2.Gypsum','3.Calcite','4.Fluorite','5.Apatite',
+        '6.Orthoclase','7.Quartz','8.Topaz','9.Corundum','10.Diamond')
+        for hardness in mohs:
+            print(hardness)
+    
+
+    mohs_scale()
+
+
+if __name__ == '__main__':
+    run()
